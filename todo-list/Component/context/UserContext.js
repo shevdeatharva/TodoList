daab87be427512fe183ad931811e5e4dded53f0f
Atharva Anil Shevde <89701795+shevdeatharva@users.noreply.github.com>
2022-05-03 18:47:48 +0530

    Add files via upload

---
+++ todo-list/Component/context/UserContext.js
@@ -0,0 +1,23 @@
+import React, {useState, useContext} from "react";
+
+const UserNameContext= React.createContext();
+const UpdateUserNameContext= React.createContext();
+
+export const useUserName=()=>{
+  return useContext(UserNameContext);
+}
+
+export const useUpdateUserName=()=>{
+  return useContext(UpdateUserNameContext);
+}
+
+export const UserProvider= ({value, children})=>{
+  const [userName, setUserName]=useState(value);
+  return(
+    <UserNameContext.Provider value={userName}>
+      <UpdateUserNameContext.Provider value={setUserName}>
+        {children}
+      </UpdateUserNameContext.Provider>
+    </UserNameContext.Provider>
+  )
+}
